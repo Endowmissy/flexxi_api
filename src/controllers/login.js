2733b397a8f49598b89c6c802102016a6b0ffac5
Endowmissy <adeolaayo31@yahoo.com>
2021-10-07 20:18:29 +0100

    feat(sign-in): Add login functionality

---
+++ src/controllers/login.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/asyncHandler');
+const sendUserResponse = require('../utils/sendUserResponse');
+
+// @desc    Login user
+// @route   POST /api/v1/auth/signin
+// @access  Public
+exports.signIn = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
+   
+    if (!user) {
+        return next(new ErrorResponse('Invalid Credentials', 401))
+    };
+    
+    const comparePassword = await user.validatePassword(password);
+   
+    if (!comparePassword) {
+        return next(new ErrorResponse('Invalid Credentials', 401))
+    };
+
+    sendUserResponse(user, 'User logged in successfully', 200, res);
+})
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { register } = require('../controllers/register');
+const { signIn } = require('../controllers/login');
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', signIn)
 
 module.exports = router;
