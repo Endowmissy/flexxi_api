12bafda344c70d0c9ae6135ad9020e434b42ccdf
Endowmissy <adeolaayo31@yahoo.com>
2021-10-07 21:20:08 +0100

    feat(login): Modify login controller and add welcome message

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,10 @@ const { signIn } = require('../controllers/login');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json({ message: "Welcome to Flexxi Products" });
+  });
+
 router.post('/register', register);
 router.post('/login', signIn)
 
